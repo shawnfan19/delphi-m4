876d6ad684727f9dcf45718a3ab84ff1e0dff284
shawnfan19 <shawnclarkefan@gmail.com>
2025-12-09 11:29:39 +0000

    detect RAP env

--- delphi/env.py
+++ delphi/env.py
@@ -1,7 +1,14 @@
 import os
 
-try:
-    DELPHI_DATA_DIR = os.environ["DELPHI_DATA_DIR"]
-    DELPHI_CKPT_DIR = os.environ["DELPHI_CKPT_DIR"]
-except KeyError as e:
-    raise EnvironmentError(f"required environment variable(s) not set: {e}")
+dx_id = os.getenv("DX_PROJECT_CONTEXT_ID")
+IN_RAP = dx_id != None
+
+if IN_RAP:
+    DELPHI_DATA_DIR = "opt/notebooks/data"
+    DELPHI_CKPT_DIR = "opt/notebooks/ckpt"
+else:
+    try:
+        DELPHI_DATA_DIR = os.environ["DELPHI_DATA_DIR"]
+        DELPHI_CKPT_DIR = os.environ["DELPHI_CKPT_DIR"]
+    except KeyError as e:
+        raise EnvironmentError(f"required environment variable(s) not set: {e}")
