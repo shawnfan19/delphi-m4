b7ad14775b56b20a9aab5d23b389116f959b140c
shawnfan19 <shawnclarkefan@gmail.com>
2026-01-14 09:56:41 +0000

    multimodal shap pipeline

--- delphi/data/shap.py
+++ delphi/data/shap.py
@@ -1,11 +1,18 @@
+from collections import defaultdict
+
 import numpy as np
 import shap
+import torch
+
+from delphi.data.utils import collate_batch, sort_by_time
+from delphi.multimodal import Modality
 
 
 class ShapMasker(shap.maskers.Masker):  # type: ignore
 
     def __init__(self):
-        pass
+        self.immutable_outputs = True
+        self.fixed_background = False
 
     def shape(self, s: tuple[np.ndarray, np.ndarray]):
         return (1, len(s[0]))
@@ -17,22 +24,196 @@ class ShapMasker(shap.maskers.Masker):  # type: ignore
         mask = self._standardize_mask(mask, s)
         x, t = s
         x, t = x.copy(), t.copy()
+        assert x.size == t.size
+        l = x.size
+        pos = np.arange(l)
 
-        # NumPy advanced indexing returns copy, not view
-        masked_x = x[~mask]
-        masked_t = t[~mask]
+        # mask: True means that a feature participates
+        masked = ~mask
+        is_event = (x > 3).copy()
+        is_female = (x == 2).copy()
+        is_male = (x == 3).copy()
+        is_last = pos == l - 1
 
-        masked_x[masked_x > 3] = 0
-        masked_t[masked_x > 3] = -1e4
+        t = np.where(is_event & masked & ~is_last, -1e4, t)
+        x = np.where(is_event & masked & ~is_last, 0, x)
 
-        masked_x[masked_x == 2] = 3
-        masked_x[masked_x == 3] = 2
+        # if the last token were to be masked, replace it with the no-event token
+        # to avoid biasing the elapsed time estimate
+        x = np.where(is_event & masked & is_last, 1, x)
 
-        x[~mask] = masked_x
-        t[~mask] = masked_t
+        x = np.where(is_male & masked, 2, x)
+        x = np.where(is_female & masked, 3, x)
 
         sort_idx = np.argsort(t)
         x = np.take_along_axis(x, sort_idx, axis=0)
         t = np.take_along_axis(t, sort_idx, axis=0)
 
         return ((x,), (t,))
+
+
+MultimodalOut = tuple[
+    np.ndarray, np.ndarray, dict[Modality, list[np.ndarray]], np.ndarray, np.ndarray
+]
+
+ShapArray = tuple[np.ndarray, np.ndarray, np.ndarray]
+
+# example:
+# MultimodalOut:
+#    x: [2, 101, 1269]
+#    t: [0, 2000, 3000]
+#    bio_x_dict: {"prs": [[1.0, -1.0, 0.5]], "wbc": [[0.0, 2.0, 0.5]]}
+#    bio_t: [0, 2500]
+#    bio_m: [2, 3]
+# ShapArray:
+#    all_x: [2, 101, 1269, 1.0, -1.0, 0.5, 0.0, 2.0, 0.5]
+#    all_t: [0, 2000, 3000, 0, 0, 0, 2500, 2500, 2500]
+#    all_m: [1, 1, 1, 2, 2, 2, 3, 3, 3]
+
+
+def to_shap_array(
+    s: MultimodalOut,
+    detokenizer: dict,
+    biomarker_features: dict[Modality, list],
+    biomarker_background: dict[Modality, np.ndarray],
+) -> tuple[ShapArray, list[str], np.ndarray]:
+
+    all_x, all_t, all_m = list(), list(), list()
+    x, t, bio_x_dict, bio_t, bio_m = s
+    all_x.append(x.astype(np.float32))
+    all_t.append(t)
+    all_m.append(np.ones_like(x))
+
+    features = list()
+    token_features = [detokenizer[int(_x)] for _x in x]
+    features.extend(token_features)
+
+    bio_bg = list()
+    # ordering ensures reproducibility
+    _, _idx = np.unique(bio_m, return_index=True)
+    modvals = bio_m[np.sort(_idx)]
+    for modval in modvals:
+        modality = Modality(modval)
+        for _bio_x, _bio_t in zip(bio_x_dict[modality], bio_t[bio_m == modval]):
+            #! for each modality, assume no more than 1 measurement at each timestep
+            all_x.append(_bio_x)
+            all_t.append(np.full_like(_bio_x, fill_value=_bio_t))
+            all_m.append(np.full_like(_bio_x, fill_value=modval))
+            _features = [f"{modality.name}.{f}" for f in biomarker_features[modality]]
+            features.extend(_features)
+            bio_bg.append(biomarker_background[modality])
+    all_x = np.concatenate(all_x)
+    all_t = np.concatenate(all_t)
+    all_m = np.concatenate(all_m)
+    if len(bio_bg) > 0:
+        bio_bg = np.concatenate(bio_bg)
+    else:
+        bio_bg = np.array([])
+
+    return (all_x, all_t, all_m), features, bio_bg
+
+
+def from_shap_array(s: ShapArray) -> MultimodalOut:
+
+    all_x, all_t, all_m = s
+    token_mask = all_m == 1
+    x, t = all_x[token_mask].astype(np.int64), all_t[token_mask].astype(np.float32)
+
+    bio_x_dict, bio_t, bio_m = dict(), list(), list()
+
+    modvals = np.unique(all_m)
+    for modval in modvals:
+        if modval <= 1:
+            continue
+        mod_mask = all_m == modval
+        mod_x, mod_t = all_x[mod_mask], all_t[mod_mask]
+        modality = Modality(modval)
+        bio_x_dict[modality] = list()
+
+        for uniq_mod_t in np.sort(np.unique(mod_t)):
+            #! for each modality, assume no more than 1 measurement at each timestep
+            bio_x_dict[modality].append(mod_x[mod_t == uniq_mod_t])
+            bio_t.append(uniq_mod_t)
+            bio_m.append(modval)
+    bio_t, bio_m = np.array(bio_t).astype(np.float32), np.array(bio_m).astype(np.int64)
+    bio_t, bio_m = sort_by_time(bio_t, bio_m)
+
+    return x, t, bio_x_dict, bio_t, bio_m
+
+
+class MultimodalShapMasker(shap.maskers.Masker):  # type: ignore
+
+    def __init__(
+        self,
+        biomarker_background: np.ndarray,
+    ):
+        self.base_masker = ShapMasker()
+        self.biomarker_background = biomarker_background
+
+        self.immutable_outputs = True
+        self.fixed_background = False
+
+    def shape(self, s: ShapArray):
+        return (1, len(s[0]))
+
+    def mask_shapes(self, s: ShapArray):
+        return [(len(s[0]),)]
+
+    def __call__(self, mask, s: ShapArray):
+        mask = self._standardize_mask(mask, s)
+        all_x, all_t, all_m = s
+        all_x, all_t, all_m = all_x.copy(), all_t.copy(), all_m.copy()
+
+        is_tok = all_m == 1
+        x, t = all_x[is_tok], all_t[is_tok]
+        all_bio_x = all_x[~is_tok]
+
+        tok_mask = mask[is_tok]
+        ((x,), (t,)) = self.base_masker(mask=tok_mask, s=(x, t))
+
+        bio_mask = mask[~is_tok]
+        all_bio_x[~bio_mask] = self.biomarker_background[~bio_mask]
+        all_x = np.concatenate([x, all_bio_x])
+
+        return (
+            (all_x,),
+            (all_t,),
+            (all_m,),
+        )
+
+
+@torch.no_grad
+def shap_forward(
+    all_x_lst: list[np.ndarray],
+    all_t_lst: list[np.ndarray],
+    all_m_lst: list[np.ndarray],
+    model,
+):
+    x_lst, t_lst = list(), list()
+    biomarker, bio_t_lst, bio_m_lst = defaultdict(list), list(), list()
+
+    for all_x, all_t, all_m in zip(all_x_lst, all_t_lst, all_m_lst):
+        x, t, bio_x_dict, bio_t, bio_m = from_shap_array((all_x, all_t, all_m))
+        x_lst.append(x)
+        t_lst.append(t)
+        for modality in bio_x_dict.keys():
+            biomarker[modality].extend(bio_x_dict[modality])
+        bio_t_lst.append(bio_t)
+        bio_m_lst.append(bio_m)
+
+    device = next(model.parameters()).device
+    idx = collate_batch(x_lst)
+    idx = torch.tensor(idx, dtype=torch.long).to(device)
+    age = collate_batch(t_lst, fill_value=-1e4)
+    age = torch.tensor(age, dtype=torch.float32).to(device)
+    for modality, bio_x_lst in biomarker.items():
+        biomarker[modality] = torch.from_numpy(np.stack(bio_x_lst)).to(device)  # type: ignore
+    mod_age = collate_batch(bio_t_lst, fill_value=-1e4)
+    mod_age = torch.tensor(mod_age, dtype=torch.float32).to(device)
+    mod_idx = collate_batch(bio_m_lst)
+    mod_idx = torch.tensor(mod_idx, dtype=torch.long).to(device)
+
+    outputs, _, _ = model.forward(idx, age, biomarker, mod_age, mod_idx)
+    logits = outputs["logits"][:, -1, :]
+
+    return logits.detach().cpu().numpy()
