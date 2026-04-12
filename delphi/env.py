1fb4c6d2f31c6e78fb5b2e77e5e19c4a7b8798e4
shawnfan19 <shawnclarkefan@gmail.com>
2026-04-12 19:55:15 +0100

    resolve delphi/env.py merge conflict

--- delphi/env.py
+++ delphi/env.py
@@ -1,5 +1,8 @@
 import os
 
+dx_id = os.getenv("DX_PROJECT_CONTEXT_ID")
+IN_RAP = dx_id != None
+
 DELPHI_DATA_READ = os.environ.get("DELPHI_DATA_DIR", "/mnt/project/data")
 DELPHI_DATA_WRITE = os.environ.get("DELPHI_DATA_DIR", "/opt/data")
 DELPHI_DATA_DIR = DELPHI_DATA_READ
